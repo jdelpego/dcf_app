35eaa4c6b8613222f500710e9a46fc4dffab8a2b
Weber Maisel <webermaisel@169-231-130-139.wireless.ucsb.edu>
2025-11-24 11:00:17 -0800

    Point frontend API to Render backend

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { SensitivityGrid } from './components/SensitivityGrid'
 import { NarrativeSection } from './components/NarrativeSection'
 import { BackendDcfV2Card } from './components/BackendDcfV2Card'
 
-const API_BASE = 'http://localhost:8001'
+const API_BASE = import.meta.env.VITE_API_BASE || "https://dcf-backend-3yq2.onrender.com/";
 
 const buildApiUrl = (path) => `${API_BASE}${path}`
 
