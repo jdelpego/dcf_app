0a187cdad0d5118a54024ac8862ef9675f423fdb
Weber Maisel <webermaisel@169-231-130-139.wireless.ucsb.edu>
2025-11-24 09:45:47 -0800

    Build static site into docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dcf_app/'
+  base: '/dcf_app/',
+  build: {
+    outDir: 'docs',            
+  },
 })
